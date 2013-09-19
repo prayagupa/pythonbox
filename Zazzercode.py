179c2b646349536a4a1bf44ccbc74f2284c2d06d
prayag <prayag@prayag.(none)>
2013-09-19 10:26:29 +0545

    ADD looper example in python OOP

---
+++ Zazzercode.py
@@ -0,0 +1,10 @@
+class Zazzercode(object):
+    def run(self):
+        print "Hello, world!"
+
+    def loop(self):
+        for number in range(1,10): 
+	    print number
+
+if __name__ == '__main__':
+    Zazzercode().loop()
