6affe72ae09a9801cf18c87629a88c32258ff84b
parayaluyanta <prayag.upd@gmail.com>
2020-12-26 18:26:57 -0800

    इन्हेरिटेन्सको सुपर सहितको उदाहरण

--- src/main/python/oo/hiking_site.py
+++ src/main/python/oo/hiking_site.py
@@ -17,6 +17,18 @@ class HikingSite:
         self.ended_at = ended
         return self
 
+
+class WalkingSite(HikingSite, object):
+    def __init__(self, started_at):
+        super(WalkingSite, self).__init__(started_at)
+
+
 if __name__ == '__main__':
+    print("normal OO: ")
     site = HikingSite("2020-12-26")
-    print site.started_at
+    print(site.started_at)
+
+    ## inheritence
+    print("Walking site: ")
+    walking_site = WalkingSite(started_at="2020-12-27")
+    print(walking_site.started_at)
